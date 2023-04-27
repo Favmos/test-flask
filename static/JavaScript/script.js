ee6b5606ec95b97e3995cc4ef668da3fda3330c7
Pemula Newbie <20200410700097@student.umj.ac.id>
2023-04-27 11:41:16 +0700

    commit pertama

---
+++ static/JavaScript/script.js
@@ -0,0 +1,20 @@
+
+var modal = document.getElementById('modal');
+
+var btn = document.getElementById('btn-modal');
+
+var span = document.getElementsByClassName('modal-close')[0];
+
+btn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+span.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
